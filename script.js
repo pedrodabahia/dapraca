a8cd01e689c0816265ffaf6f34ae0dedc5004c67
Pedro Matos <109925892+pedrodabahia@users.noreply.github.com>
2025-02-19 16:39:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var form = document.querySelector("form");
-
+    var msg = document.querySelector("#msg");
     if (!form) {
         console.error("Formulário não encontrado!");
         return;
@@ -8,17 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     form.addEventListener("submit", async function (event) {
         event.preventDefault(); // Evita que o formulário seja enviado mais de uma vez
-
+        
         var cidadeValor = document.getElementById("cidade")?.value || "";
-
-        if (cidadeValor === "Itabatã") {
-            cidadeValor = "Itabata";
-        }
-
         localStorage.setItem("cidade", cidadeValor);
 
         var nome = document.getElementById("nome")?.value || "Sem Nome";
         var idade = document.getElementById("idade")?.value || "Sem Idade";
+
+        if(cidadeValor != "nulo" && nome != "Sem Nome"  && idade != "Sem Idade"){
 
         var data = {
             data: {
@@ -28,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         };
 
+        msg.style.top = "0vw";
         try {
             let response = await fetch("https://script.google.com/macros/s/AKfycbwKJqK9W1OTL-7yA0QCnjHK0EI4-AO745BNTfR1o_y_PHnERB5VBnALtL24DgFOl9cF/exec", {
                 method: "POST",
@@ -35,28 +33,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data)
             });
+        msg.style.top = "-7vw";
 
 
 
                             switch(data.data.cidade) {
           case "Posto da Mata":
-            window.location.href = "https://chat.whatsapp.com/J7vSp3d4mwPBJaEmbStoPP";
+            window.location.href = "https://chat.whatsapp.com/CWsObLw5pEgA25KGnJOkg7";
             localStorage.clear();
             break;
           case "Itabatã":
-            window.location.href = "https://chat.whatsapp.com/Gnc21qUogOa5NYGzR033sM";
+            window.location.href = "https://chat.whatsapp.com/ISYuaj7DK9X3cPb5LrX7wl";
             localStorage.clear();
             break;
           case "Mucuri":
-            window.location.href = "https://chat.whatsapp.com/JVkyD6mW1dXKNvUDAmjGDy";
+            window.location.href = "https://chat.whatsapp.com/DCMyS5jV5n82aszyaCGw3C";
             localStorage.clear();
             break;
           case "Prado":
-            window.location.href = "https://chat.whatsapp.com/KZOO4rSC85aIF465IRE8Pg";
+            window.location.href = "https://chat.whatsapp.com/HoQNQks02SYDsFSiWjr6qR";
             localStorage.clear();
             break;
           case "Itamaraju":
-            window.location.href = "https://chat.whatsapp.com/GEz8Y8lv8iP199IG5WXQkr";
+            window.location.href = "https://chat.whatsapp.com/IfZwT8awuhy18SrZE8dB2s";
             localStorage.clear();
             break;
         }
@@ -71,5 +70,15 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Erro ao enviar dados:", error);
             alert("Erro de conexão. Tente novamente.");
         }
+    }else{
+        msg.style.backgroundColor = "red";
+        msg.innerText = "Porfavor, Preencha todos os campos!";
+        msg.style.top = "0vw";
+        setTimeout(() => {
+        msg.style.top = "-7vw";
+
+        },3000);
+        console.log(`${nome} , ${idade}, ${cidadeValor}`)
+    }
     });
 });
